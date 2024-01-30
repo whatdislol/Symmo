f40577d7b0e69e3bd268ff1f293d8fb42be566e8
cenpnt <cenpolarbears@gmail.com>
2024-01-30 21:35:58 +0700

    test again

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "commit goodbye world :(";
+    std::cout << "commit goodbye world :(:D:D:D::D:D:D:D";
+    std::cout << "commit goodbye world :(:P:P:P:P:P:P:P:P";
 
     return 0;
 }
