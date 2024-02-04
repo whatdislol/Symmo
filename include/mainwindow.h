401547b956ef6478bb17d5b988d711df9e7c7de3
whatdislol <neunglert@gmail.com>
2024-02-04 22:50:01 +0700

    Add frames for each section and icons for the player

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,21 +2,39 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtCore>
+#include <QtWidgets>
+#include <QtMultimedia>
+#include <QFileDialog>
+#include <QStyle>
 
+QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
-};
+private slots:
+    void on_pushButton_Volumn_clicked();
 
+    void on_pushButton_Play_clicked();
+
+    void on_actionAdd_Fiel_triggered();
+
+    void on_horizontalSlider_song_duration_valueChanged(int value);
+
+private:
+    Ui::MainWindow* ui;
+    bool is_muted = false;
+    bool is_pause = false;
+    QMediaPlayer* M_Player;
+};
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,76 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QIcon>
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    int iconSize = 25;
+
+    QIcon playIcon = style()->standardIcon(QStyle::SP_MediaPlay);
+    QIcon skipIcon = style()->standardIcon(QStyle::SP_MediaSeekForward);
+    QIcon backIcon = style()->standardIcon(QStyle::SP_MediaSeekBackward);
+    QIcon volumnIcon = style()->standardIcon((QStyle::SP_MediaVolume));
+
+    // Get the actual size of the icon without scaling
+    QSize actualSize = playIcon.actualSize(QSize(iconSize, iconSize));
+
+    // set size for button
+    ui->pushButton_Play->setIcon(playIcon);
+    ui->pushButton_Play->setIconSize(actualSize);
+    ui->pushButton_Skip->setIcon(skipIcon);
+    ui->pushButton_Skip->setIconSize(actualSize);
+    ui->pushButton_Back->setIcon(backIcon);
+    ui->pushButton_Back->setIconSize(actualSize);
+    ui->pushButton_Volumn->setIcon(volumnIcon);
+    ui->pushButton_Volumn->setIconSize(actualSize);
+
+    M_Player = new QMediaPlayer();
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_Volumn_clicked()
+{
+    if (is_muted == false) {
+        ui->pushButton_Volumn->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+        is_muted = true;
+    }
+    else {
+        ui->pushButton_Volumn->setIcon(style()->standardIcon(QStyle::SP_MediaVolumeMuted));
+        is_muted = false;
+    }
+}
+
+
+void MainWindow::on_pushButton_Play_clicked()
+{
+    if (is_pause == false) {
+        ui->pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+        is_pause = true;
+    }
+    else {
+        ui->pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+        is_pause = false;
+    }
+}
+
+
+void MainWindow::on_actionAdd_Fiel_triggered()
+{
+
+}
+
+
+void MainWindow::on_horizontalSlider_song_duration_valueChanged(int value)
+{
+
 }
